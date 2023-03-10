ea3895beb75340cdb84b7021a52b587b678398ae
Hantzo Studios <haon.00@gmail.com~>
2023-03-11 00:01:05 +0100

    terminada

---
+++ ContactoKopilanServiciosGraficos.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Contacto Kopilan comunicacion grafica</title>
+<meta name="description" content="Para ponerse en contacto con nuestra empresa, Kopilan Comunicacion Grafica dispone de teléfono, formulario de contacto o cuenta de correo" />
+<meta name="keywords" content="contacto kopilan, contacto kopilan comunicación grafica, contacto comunicación grafica, " />
+<link rel="shortcut icon" href="http://www.kopilan.com/icokopilan.png"/>
+<link href="kopistyle.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+ <form id="form1" name="ContactoKopilanServiciosGraficos.php" method="post" action="../ContactoKopilanServiciosGraficos.php">
+      <?
+if (!$_POST){
+?>
+ <div>
+   <div class="idioma"> <a title="cambiar idioma a Euskara" accesskey="1" href="Euskeraz/KopilanKomunikazioGrafikoa.html" rel="Section">Euskara</a> &#8212; <a title="cambiar idioma a Castellano" href="index.html" rel="Section">Castellano</a></div>
+   <h1><span class="titulo2">K</span><span class="titulo">opilan</span><span class="titulo"></span><span class="subtitulo"> Comunicacion Grafica</span></h1>
+</div>
+ <div class="menus2"> <a title="Volver a inicio" accesskey="2" href="index.html" rel="Section">&nbsp;Inicio </a>|<a title="diseño grafico y diseño corporativo" accesskey="3" href="CreacionesGraficas.html" rel="Section"> Diseño Grafico </a>|<a title="ir a servicios de impresion" accesskey="4" href="ServiciosImpresion.html" rel="Section"> Servicios Impresión </a>|<a title="diseño web, posicionamiento SEO, accesibilidad" accesskey="5" href="WebDesingSEO.html" rel="Section"> Diseño Web </a>|<a title="ir a mapa web" accesskey="6" href="KopilanWebMapa.html" rel="Section"> Mapa web&nbsp;</a></div>
+ <div class="titulostexto">
+  <h2>Contacto Kopilan</h2>
+</div>
+
+<div class="cajacontenidos"> <img src="imagenes/kopilantarjeta2011.jpg" alt="Tarjeta Kopilan 2011" name="kopilantarjeta2011" width="450" height="293" class="floatderecha" id="kopilantarjeta2011" /> 
+<p>
+No dude en solicitar información y consultar presupuestos a través de nuestro formulario web, escribiendo a traves de Email o por consulta telefónica.</p>
+<p>
+Atención al público: 
+L-J: de 09:00 a 13:00 - de 15:00 a 18:00. V: de 09:00 a 14:00)
+</p>
+<ul>
+  <li>Direccion:</li>
+  <li class="ul1">Ibaitarte 5,</li>
+  <li class="ul1">20870 Elgoibar</li>
+  <li class="ul1">Gipuzkoa</li>
+</ul>
+<ul>
+  <li>Teléfono fijo: <span class="ul1">943 743 551</span>  </li>
+  <li class="celdaizq"><a href="mailto: kopilan@kopilan.com"><strong>kopilan@kopilan.com</strong></a></li>
+</ul>
+<ul>
+  <li>Tambien puede usar el nuestro formulario de contacto.</li>
+
+</ul>
+
+
+
+
+<div><table>
+  <tr>
+      <td colspan="2"><h3>Formulario de contacto</h3></td>
+    </tr>
+    <tr>
+      <td><label for="nom"></label>
+        <input name="nom" type="text" id="nom" value="Nombre" /></td>
+      <td><label for="ape"></label>
+        <input name="ape" type="text" id="ape" value="Apellidos" size="50" maxlength="50" /></td>
+      </tr>
+    <tr>
+      <td><label for="tlf"></label><input name="tlf" type="text" id="tlf" value="Tlf" /></td>
+      <td><label for="dir"></label>
+        <input name="dir" type="text" id="dir" value="Direccion" size="50" maxlength="50" /></td>
+      </tr>
+    <tr>
+      <td><label for="cp"></label>
+        <input name="cp" type="text" id="cp" value="C.P." size="5" maxlength="5" /></td>
+      <td><label for="mai"></label>
+        <input name="mai" type="text" id="mai" value="E-Mail" size="50" maxlength="50" />      </td>
+      </tr>
+    <tr>
+      <td><label for="loc"></label>
+        <input name="loc" type="text" id="loc" value="Localidad" /></td>
+      <td><label for="pro"></label>
+        <label for="pro"></label>
+        <select name="pro" id="pro">
+          
+          <option value="acor">A Coruña</option>
+          <option value="alav">Álava</option>
+          <option value="alba">Albacete</option>
+          <option value="alic">Alicante</option>
+          <option value="alme">Almería</option>
+          <option value="astu">Asturias</option>
+          <option value="avil">Ávila</option>
+          <option value="bada">Badajoz</option>
+          <option value="bale">Baleares</option>
+          <option value="barc">Barcelona</option>
+          <option value="bizk">Bizkaia</option>
+          <option value="burg">Burgos</option>
+          <option value="cace">Cáceres</option>
+          <option value="cadi">Cádiz</option>
+          <option value="cant">Cantabria</option>
+          <option value="cast">Castellón</option>
+          <option value="ceut">Ceuta</option>
+          <option value="ciud">Ciudad Real</option>
+          <option value="corb">Córdoba</option>
+          <option value="cuen">Cuenca</option>
+          <option value="giro">Girona</option>
+          <option value="gran">Granada</option>
+          <option value="guad">Guadalajara</option>
+          <option value="gipu">Gipuzkoa</option>
+          <option value="huel">Huelva</option>
+          <option value="hues">Huesca</option>
+          <option value="jaen">Jaén</option>
+          <option value="lasp">Las Palmas</option>
+          <option value="leon">León</option>
+          <option value="llei">Lleida</option>
+          <option value="lari">La Rioja</option>
+          <option value="lugo">Lugo</option>
+          <option value="madr">Madrid</option>
+          <option value="mala">Málaga</option>
+          <option value="meli">Melilla</option>
+          <option value="murc">Murcia</option>
+          <option value="nava">Navarra</option>
+          <option value="oure">Ourense</option>
+          <option value="pale">Palencia</option>
+          <option value="pont">Pontevedra</option>
+          <option value="sala">Salamanca</option>
+          <option value="sant">Santa Cruz de Tenerife</option>
+          <option value="sego">Segovia</option>
+          <option value="sevi">Sevilla</option>
+          <option value="sori">Soria</option>
+          <option value="tarr">Tarragona</option>
+          <option value="teru">Teruel</option>
+          <option value="tole">Toledo</option>
+          <option value="vale">Valencia</option>
+          <option value="vall">Valladolid</option>
+          <option value="zamo">Zamora</option>
+          <option value="zara">Zaragoza</option>
+          <option value="prob" selected="selected">Provincia</option>
+        </select></td>
+      </tr>
+    <tr>
+      <td align="right" valign="top"></td>
+      <td><label for="come"></label><textarea name="come" cols="48" rows="5" id="come">Comentarios</textarea>
+      <br />
+      <input type="submit" name="enviar" id="enviar" value="Enviar" />
+      <input type="reset" name="enviar2" id="enviar2" value="Restablecer" /></td>
+    </tr>
+   
+  </table>
+  </div>
+  </div>
+  <div class="textobajo">&nbsp;</div>
+  <div class="menus"><a title="ver promociones de imprenta" href="PromocionesImprenta.html" rel="Section">&nbsp;Promociones </a>|<a title="conoce el equipo creativo" href="EquipoCreativo.html" rel="Section"> Equipo </a>|<a title="visita nuestra galeria" accesskey="9" href="Galeria.html" rel="Section"> Galeria </a>|<a title="mira donde estamos" accesskey="7"  href="LocalizacionKopilanComunicacionGrafica.html" rel="Section"> Localizacion </a>|<a title="enlaces de interes" href="Enlaces.htm" rel="Section"> Enlaces </a>|<a title="contacta con nosotros" accesskey="8" href="ContactoKopilanServiciosGraficos.php" rel="Section"> Contacto&nbsp;</a></div>
+  <hr />
+  <div class="celdaizq">
+    <div class="floatderecha3"> 2011-2012© all rights reserved - <a title="aviso legal" href="AvisoLegal.html" rel="Section">Aviso legal</a> - <a title="consulta la guia de accesibilidad" accesskey="0" href="GuiaDeAccesibilidad.html" rel="Section">Guia de accesibilidad</a></div>
+    Ibaitarte 5, 20870 Elgoibar (Gipuzkoa) Tel:943743551 <a title="enviar con gestor de correo" href="mailto:kopilan@kopilan.com">kopilan@kopilan.com</a></div>
+  <div class="validadores" id="apDiv11"> <a title="validar Xhtml" href="http://validator.w3.org/check?uri=referer" target="_blank" rel="external"><img src="imagenes/valid-xhtml10.gif" alt="Valid XHTML 1.0 Transitional" /></a> &#8226; <a title="validar CSS" href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank" rel="external"><img src="imagenes/vcss.gif" alt="¡CSS Válido!"/></a> </div>
+ </form>
+
+<?
+}else{
+	$cuerpo="Formulario enviado\n";
+	$cuerpo.="Nombre:".$_POST["nom"]."\n";
+	$cuerpo.="Apellidos:".$_POST["ape"]."\n";
+	$cuerpo.="Direccion:".$_POST["dir"]."\n";
+	$cuerpo.="C.P.:".$_POST["cp"]."\n";
+	$cuerpo.="TFN:".$_POST["tlf"]."\n";
+	$cuerpo.="Localidad:".$_POST["loc"]."\n";
+	$cuerpo.="Provincia:".$_POST["pro"]."\n";
+	$cuerpo.="Mail:".$_POST["mai"]."\n";
+	$cuerpo.="Comentarios:".$_POST["come"]."\n";
+	
+	
+	mail("kopilan@kopilan.com","Formulario recibido de la web",$cuerpo);
+		
+	echo"gracias por rellenar el formulario. Se ha enviado correctamente.";
+}
+?>
+ 	
+
+</body>
+</html>
